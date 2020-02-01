790ba620ecf456b2a66450e8da068103859a1126
Siliang Chang <siliang.chang@gmail.com>
2020-02-01 16:28:40 -0700

    folder rearrangement

---
+++ 1-basics/app.js
@@ -0,0 +1,10 @@
+// core modules
+// http, https, fs, path, os
+const http = require("http");
+const routes = require("./routes");
+
+console.log(routes.someText);
+
+const server = http.createServer(routes.handler);
+
+server.listen(3000);
