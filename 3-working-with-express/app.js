4abff7cc4cb0a40dc87efd20208516a5e3b3f004
Siliang Chang <siliang.chang@gmail.com>
2020-02-09 22:23:42 -0700

    5: added user form

--- 3-working-with-express/app.js
+++ 3-working-with-express/app.js
@@ -1,20 +1,34 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 // an event handler
 const app = express();
 
 // if want to add a middleware for all request
-app.use("/", () => {
-  console.log("This always runs");
+app.use("/", (req, res, next) => {
+  // console.log("This always runs");
   next();
 });
 
+// adding a parser, this should be done before all routing
+// registers a middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use("/add-product", (req, res, next) => {
-  console.log("In the next middleware!");
-  res.send("<h1>The 'add product' page</h1>");
+  // console.log("In the next middleware!");
+  res.send(
+    "<form action='/product' method='POST'><input type='text' name='title'><button>Add Product</button></form>"
+  );
 });
+
+app.use("/product", (req, res, next) => {
+  // req.body=undefined it doesn't parse automatically
+  console.log(req.body);
+  res.redirect("/");
+});
+
 // use() allows to add new middleware function
 app.use("/", (req, res, next) => {
-  console.log("In the next middleware!");
+  // console.log("In the next middleware!");
   // header is set by express by default to Content-Type: text/html; charset=utf-8
   // send source code: https://github.com/expressjs/express/blob/master/lib/response.js
   res.send("<h1>Hello from express!</h1>");
